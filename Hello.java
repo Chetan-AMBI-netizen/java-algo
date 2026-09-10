2054ec1b7d0a67f7df23a6f2dca83d31e02967a8
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-11 01:13:07 +0530

    completing the Datatypes in .java like a instance variables with

--- Hello.java
+++ Hello.java
@@ -5,12 +5,16 @@ public class Hello {
         String Na_me = "Chetan";
         char section = 'A';
         int semester = 3;
-
+        long PH = 910886;
         float $SGPA = 80;
-        
+        double price = 99.99;
+        int arr[] = {1,2,3,4,5};
+         System.out.println(arr[5]);
+         System.out.println(price);
         System.out.println(Na_me);
          System.out.println(section);
           System.out.println(semester);
+           System.out.println(PH);
            System.out.println($SGPA);
         
 
