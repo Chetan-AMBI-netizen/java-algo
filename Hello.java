3012143ed222b19bcdd5e7663a616e04ac22fa1f
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-13 03:04:59 +0530

    Datatypes

--- Hello.java
+++ Hello.java
@@ -1,27 +1,17 @@
-public class Hello {
-
-
+public class Hello{
     public static void main(String[] args) {
-        String Na_me = "Chetan";
-        char section = 'A';
-        int semester = 3;
-        long PH = 910886;
-        float $SGPA = 80;
-        double price = 99.99;
-        int arr[] = {1,2,3,4,5};
-         System.out.println(arr[5]);
-         System.out.println(price);
-        System.out.println(Na_me);
-         System.out.println(section);
-          System.out.println(semester);
-           System.out.println(PH);
-           System.out.println($SGPA);
+        int x;
+        for(x=1; x<=10; x++)
+        {
+            if(x % 2==0){
+                System.out.println("even number:" +x);
+            }
+            else{
+                System.out.println("odd number" +x);
+            }
+        }
+          
         
-
-
-
-        
-
 
     }
 }
