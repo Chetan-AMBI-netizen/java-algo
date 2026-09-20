8a880629c643c409d5ea129461adf0951f4d7034
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-20 14:52:30 +0530

    started to work on

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+    public static void main(String[] args){
+        //relation operators//
+        int x = 9;
+        int y = 10;
+        double a = 11;
+        double b = 14;
+        boolean result = x<y;
+        System.out.println(result);
+        
+
+
+    }
+    
+}
