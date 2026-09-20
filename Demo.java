c530661275306d3c7413d545d0335716f41b3965
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-20 15:11:46 +0530

    done with all operators and conditional statments

--- Demo.java
+++ Demo.java
@@ -1,15 +1,28 @@
 public class Demo {
     public static void main(String[] args){
-        //relation operators//
-        int x = 9;
-        int y = 10;
-        double a = 11;
-        double b = 14;
-        boolean result = x<y;
+        /*conditional statements//
+        int a = 100;
+        if(a>99)
+            System.out.println("correct");
+        else if(a==100)
+            System.out.println("might be");
+        else
+            System.out.println("wrong");
+        */
+        //relation and logic operators//
+        int x = 8;
+        int y = 12;
+        float c = 9.8f;
+        String name = "Chetan";
+    
+
+        System.out.println("full name is:-" +name   + "Ambiger" );
+       // System.out.println(result);
+        //ternary operator//
+
+        int result =0;
+        result = y>=x ? 10 : 20;
         System.out.println(result);
-        
-
-
     }
     
 }
