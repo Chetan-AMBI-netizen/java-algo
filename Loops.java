d71f26894d665182f3ebc995ebebdf2600f857f5
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-21 22:51:31 +0530

    today work target done

--- Loops.java
+++ Loops.java
@@ -1,6 +1,6 @@
 public class Loops {
 public static void main(String[] args) {
-   // int i=2;
+    int i=2;
    /*  while(i<10){
 
         System.out.println("hi " + i);
@@ -20,7 +20,7 @@ public static void main(String[] args) {
     System.out.println("hello guys "  +  i);
     i++;
    }while(i<=9);
-   */
+   
   for(int i=1; i<=5; i++)
   {
     System.out.println(" Day "  +   i);
@@ -28,9 +28,13 @@ public static void main(String[] args) {
    {
     System.out.println(" "  +  (j+8) + " - "  +(j+9));
    }
-   for (int k = 10; k%2==0&& k<=10;k++){
+   for (int k = 8;  k%2==0&& k<=10;k++){
     System.out.println("break times "  +   (k  +  3)  + "   -  " + ( k + 4 ));
    }
+    */
+  for(; i<=8;){
+    System.out.println(i);
+    i++;
 
   }
 }
